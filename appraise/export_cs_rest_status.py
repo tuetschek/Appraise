dc1b94f5001581b3d4b41adfcede344299836eba
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2017-01-27 00:06:34 +0100

    Basically a copy of WMT16 stuff

---
+++ appraise/export_cs_rest_status.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Project: Appraise evaluation system
+ Author: Christian Federmann <cfedermann@gmail.com>
+
+usage: export_cs_rest_status.py
+
+Exports HIT status for all language pairs.
+
+"""
+from datetime import datetime
+import os
+import sys
+
+
+if __name__ == "__main__":
+    # Properly set DJANGO_SETTINGS_MODULE environment variable.
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    PROJECT_HOME = os.path.normpath(os.getcwd() + "/..")
+    sys.path.append(PROJECT_HOME)
+    
+    # We have just added appraise to the system path list, hence this works.
+    from appraise.cs_rest.models import HIT, LANGUAGE_PAIR_CHOICES
+    
+    remaining_hits = {}
+    for language_pair in [x[0] for x in LANGUAGE_PAIR_CHOICES]:
+        remaining_hits[language_pair] = HIT.compute_remaining_hits(
+          language_pair=language_pair)
+    
+    print
+    print '[{0}]'.format(datetime.now().strftime("%c"))
+    for k, v in remaining_hits.items():
+        print '{0}: {1:03d}'.format(k, v)
+    print
